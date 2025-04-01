36a1191235187c6a61583b38132430485d6bea10
louayamor <amor.louay20@gmail.com>
2025-04-01 13:22:48 +0100

    Initialized AuthGate

---
+++ src/main/java/itbs/louayamor/authgate/AuthGateApplication.java
@@ -0,0 +1,13 @@
+package itbs.louayamor.authgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itbs/louayamor/authgate/AuthGateApplicationTests.java
@@ -0,0 +1,13 @@
+package itbs.louayamor.authgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
